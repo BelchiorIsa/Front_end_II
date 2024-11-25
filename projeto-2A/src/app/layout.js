2177fc8b097fa49341d61e714421ade296577fcc
BelchiorIsa <anaisabelyribeiro43@gmail.com>
2024-11-25 11:02:15 -0400

    save

--- projeto-2A/src/app/layout.js
+++ projeto-2A/src/app/layout.js
@@ -1,13 +1,13 @@
-import localFont from "next/font/local";
+
 import "./globals.css";
 import Header from "@/components/header";
 
 export const metadata = {
-  title: 'Homem Aranha',
-  description: 'Meu nome é Isabely, mas eu me identifico como Homem-aranha',
+  title: "Primeira aplicação",
+  description: "Aplicação de front-end II",
   charset: 'UTF-8',
-  author: 'Isabely Belchior',
-  keywords: 'HTML, CSS, JavaScript, React, Next.js',
+  author: 'Ana Isabely',
+  keywords: 'HTML, CSS, JavaScript, React, Next.js'
 };
 
 export default function RootLayout({ children }) {
--- projeto-2A/src/components/header/index.js
+++ projeto-2A/src/components/header/index.js
@@ -1,25 +1,42 @@
 'use client';
-import Link from "next/link"
-import { useState} from "react";
+import Link from "next/link";
 import styles from "./header.module.css"
-export default function Header(){
-    const [showMenu, setShowMenu] = useState(false)//aparece, e true some
-    return(
+import { useState } from "react";
+import Image from "next/image";
+
+
+
+export default function Header() {
+    const [showMenu, setShowMenu] = useState(true)
+    return (
         <header>
-            <button onClick={()=>setShowMenu(!showMenu)}>Clique</button>
+            <button onClick={() => setShowMenu(!showMenu)}>
+                <Image className={styles.rat}
+                    src="/menu.png"
+                    alt="Ícone do menu"
+                    width={30}
+                    height={30}
+
+                />
+            </button>
             {
-                showMenu && //&& (define se mostra a nav a partir do booleano) 
+
+                showMenu &&
+
                 <nav>
-                <ul className={styles.li}>
-                    <li><Link href='/'>Home</Link></li>
-                    <li><Link href='/sobre'>Sobre</Link></li>
-                    <li><Link href='/contato'>Contato</Link></li>
-                    
-                </ul>
-                
-            </nav>
+                    <ul className={styles.li}>
+                        <img className={styles.me}
+                            src="/BD2.png"
+                            alt="menu"
+                            width={75}
+                            height={40}
+                        />
+                        <li><Link href='/'>Home</Link></li>
+                        <li><Link href='/sobre'>Sobre</Link></li>
+                        <li><Link href='/contato'>Contato</Link></li>
+                    </ul>
+                </nav>
             }
         </header>
-            
     )
-}
+} 
