2bf712453472bf017920f4e1506febeb73ce4ed9
BelchiorIsa <anaisabelyribeiro43@gmail.com>
2025-02-04 09:50:19 -0400

    a

--- projeto_teste/projeto_teste/src/app/page.js
+++ projeto_teste/projeto_teste/src/app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 export default function Page() {
   return (
     <div className={styles.title}>
-      <h1>Seja bem-vindo à Clínica Belchior</h1>
+      <h1>Seja bem-vindo a Clínica Belchior</h1>
     </div>
   );
 }
