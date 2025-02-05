e7a36e5cb8f6dc017fdf222c6fa0dc8bc07a54ec
BelchiorIsa <anaisabelyribeiro43@gmail.com>
2025-02-05 08:06:01 -0400

    updates

--- projeto_teste/projeto_teste/src/app/layout.js
+++ projeto_teste/projeto_teste/src/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'; 
-import Header from '../components/Header'; 
+import Header from '@/components/Headers'; 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt">
