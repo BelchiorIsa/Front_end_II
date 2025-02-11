c57d2591516827c3b8a6bc8d5a0ef1ed2ee6bd50
Belchiorisa <ana.isabely@estudante.ifro.edu.br>
2025-02-10 20:11:02 -0400

    a

--- projeto_teste/projeto_teste/src/app/page.js
+++ projeto_teste/projeto_teste/src/app/page.js
@@ -4,6 +4,13 @@ export default function Page() {
   return (
     <div className={styles.title}>
       <h1>Seja bem-vindo a Clínica Belchior</h1>
+      <p>Aqui, sua saúde e bem-estar são a nossa prioridade. Com um atendimento humanizado, acolhedor e de qualidade, garantimos que você se sinta confortável e cuidado em todas as etapas da sua jornada de saúde.
+
+Nossa equipe de profissionais altamente qualificados está pronta para oferecer os melhores tratamentos e orientações personalizadas para você e sua família. Se você busca atenção especializada, confiança e cuidado de excelência, você está no lugar certo!
+
+Na Clínica Belchior, seu sorriso e saúde nos movem a cada dia. Venha viver a experiência de um atendimento único, onde a sua saúde é tratada com o carinho que você merece.
+
+</p>
     </div>
   );
 }
